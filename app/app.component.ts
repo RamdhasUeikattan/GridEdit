68d9f697bffe1673fc12942fdb191271da36e513
Ramdhas Ueikattan <ramdhas.ueikattan@syncfusion.com>
2017-09-12 17:15:31 +0530

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,8 @@ import { ToolbarService, EditService, PageService } from '@syncfusion/ej2-ng-gri
 
 @Component({
     selector: 'my-app',
-    template: `<ej-grid [dataSource]='data' allowPaging='true' [pageSettings]='pageSettings' [editSettings]='editSettings' [toolbar]='toolbar'>
+    template: `<ej-grid [dataSource]='data' allowPaging='true' [pageSettings]='pageSettings' [editSettings]='editSettings' [toolbar]='toolbar'
+    (actionComplete)='actionComplete($event)'>
                 <e-columns>
                     <e-column field='OrderID' headerText='Order ID' width='120' textAlign="right" isPrimaryKey='true' [validationRules]='orderidrules'></e-column>
                     <e-column field='CustomerID' headerText='Customer ID' width='120' textAlign="right" [validationRules]='customeridrules'></e-column>
@@ -14,8 +15,6 @@ import { ToolbarService, EditService, PageService } from '@syncfusion/ej2-ng-gri
                     <e-column field='ShipCountry' headerText='Ship Country' width='150'></e-column>
                 </e-columns>
               </ej-grid>`,
-    styleUrls: ['app/app.style.css'],
-    encapsulation: ViewEncapsulation.None,
     providers: [ToolbarService, EditService, PageService]
 })
 export class AppComponent implements OnInit {
@@ -31,13 +30,24 @@ export class AppComponent implements OnInit {
     public freightrules: Object;
     public pageSettings: Object;
     public editparams: Object;
+    public modifiedData: {edited: Object[], deleted: Object[] } = {edited: [], deleted: []};
 
     public ngOnInit(): void {
         this.data = gridData1;
-        this.editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'dialog' };
+        this.editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true };
         this.toolbar = ['add', 'edit', 'delete', 'update', 'cancel'];
         this.orderidrules = { required: true };
         this.customeridrules = { required: true };
         this.pageSettings = { pageCount: 5};
     }
+    actionComplete(args){
+        switch (args.requestType){
+           case 'save':
+              this.modifiedData.edited.push(args.data);
+           break;
+           case 'delete':
+              this.modifiedData.deleted = this.modifiedData.deleted.concat(args.data);
+           break;
+        }
+  }
 }
