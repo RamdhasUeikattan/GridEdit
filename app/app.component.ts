8ce95446a5dc49f17373bf4f92c2a5bdbc6b0f5b
Ramdhas Ueikattan <ramdhas.ueikattan@syncfusion.com>
2017-10-24 16:05:41 +0530

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { gridData1 } from './data';
-import { Grid } from '@syncfusion/ej2-grids';
-import { ToolbarService, EditService, PageService } from '@syncfusion/ej2-ng-grids';
+import { ToolbarService, EditService, PageService, GridComponent } from '@syncfusion/ej2-ng-grids';
 
 @Component({
     selector: 'my-app',
-    template: `<ej-grid [dataSource]='data' allowPaging='true' [pageSettings]='pageSettings' [editSettings]='editSettings' [toolbar]='toolbar'
+    template: `<ej-grid [dataSource]='data' [allowPaging]='true' [pageSettings]='pageSettings' [editSettings]='editSettings' [toolbar]='toolbar'
     (actionComplete)='actionComplete($event)'>
                 <e-columns>
-                    <e-column field='OrderID' headerText='Order ID' width='120' textAlign="right" isPrimaryKey='true' [validationRules]='orderidrules'></e-column>
+                    <e-column field='OrderID' headerText='Order ID' width='120' textAlign="right" [isPrimaryKey]='true' [validationRules]='orderidrules'></e-column>
                     <e-column field='CustomerID' headerText='Customer ID' width='120' textAlign="right" [validationRules]='customeridrules'></e-column>
                     <e-column field='Freight' headerText='Freight' width='120' format='C2' textAlign="right" editType='numericedit'></e-column>
                     <e-column field='ShipName' headerText='Ship Name' width='170'></e-column>
@@ -22,9 +21,9 @@ import { ToolbarService, EditService, PageService } from '@syncfusion/ej2-ng-gri
 export class AppComponent implements OnInit {
 
     @ViewChild('grid')
-    public grid: Grid;
+    public grid: GridComponent;
 
-    public data: Object[];
+    public data: any;
     public editSettings: Object;
     public toolbar: string[];
     public orderidrules: Object;
@@ -32,7 +31,7 @@ export class AppComponent implements OnInit {
     public freightrules: Object;
     public pageSettings: Object;
     public editparams: Object;
-    public modifiedData: {edited: Object[], deleted: Object[] } = {edited: [], deleted: []};
+    public modifiedData: {edited: Object[], deleted: Object[], added: Object[] } = {edited: [], deleted: [], added:[]};
 
     public ngOnInit(): void {
         this.data = gridData1;
@@ -42,13 +41,18 @@ export class AppComponent implements OnInit {
         this.customeridrules = { required: true };
         this.pageSettings = { pageCount: 5};
     }
-    actionComplete(args){
+    actionComplete(args) {
+        console.log(args.requestType + '--' + args.action );
         switch (args.requestType){
            case 'save':
-              this.modifiedData.edited.push(args.data);
+                if(args.action === 'add' && args.data){
+                    this.modifiedData.added.push(args.data);
+                } else {
+                    this.modifiedData.edited.push(args.data);
+                }
            break;
            case 'delete':
-              this.modifiedData.deleted = this.modifiedData.deleted.concat(args.data);
+              this.modifiedData.deleted.push(...args.data);
            break;
         }
   }
